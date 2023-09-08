5dc2c5c76d34960aa64adc41b34dd6d2d7c66997
lutsiu <oleksandr.lutsiuk.1603@gmail.com>
2023-09-08 10:41:21 +0200

    chat search component is done

--- frontend/src/components/Chat/Overlays/SearchOverlay/index.tsx
+++ frontend/src/components/Chat/Overlays/SearchOverlay/index.tsx
@@ -1,17 +1,52 @@
 import { motion } from "framer-motion";
-import { HiMagnifyingGlass, HiArrowLeft } from "react-icons/hi2";
-import { AiOutlineCalendar } from "react-icons/ai";
-import { IoMdClose } from "react-icons/io";
-import { mediumPurple, gray } from "../../../../utils/colors";
+
 import { useState } from "react";
+import Header from "./Header";
 interface Props {
   showSearch: boolean;
   setShowSearch: (show: boolean) => void;
 }
 export default function SearchOverlay(props: Props) {
   const { showSearch, setShowSearch } = props;
-  const [inputIsFocused, setInputIsFocused] = useState(false);
+
   const [query, setQuery] = useState("");
+
+  const messages: {
+    sender: string;
+    date: string;
+    content: string;
+    profileImg: string;
+  }[] = [
+    {
+      sender: "Walter",
+      date: "Jul, 4",
+      content: "Jesse, Jesseeee",
+      profileImg:
+        "https://i.kym-cdn.com/entries/icons/facebook/000/031/673/hank_died_walt_cries_(breaking_bad_spoilers)_1-35_screenshot.jpg",
+    },
+    {
+      sender: "Jesse",
+      date: "Jul, 4",
+      content: "Hey yo Mista Wite, sssup?",
+      profileImg:
+        "https://ih0.redbubble.net/image.2289245060.4528/raf,360x360,075,t,fafafa:ca443f4786.jpg",
+    },
+    {
+      sender: "Walter",
+      date: "Jul, 4",
+      content: "Jesse, I need you, RIGHT NOW!",
+      profileImg:
+        "https://i.kym-cdn.com/entries/icons/facebook/000/031/673/hank_died_walt_cries_(breaking_bad_spoilers)_1-35_screenshot.jpg",
+    },
+    {
+      sender: "Jesse",
+      date: "Jul, 4",
+      content: "No, beeatch",
+      profileImg:
+        "https://ih0.redbubble.net/image.2289245060.4528/raf,360x360,075,t,fafafa:ca443f4786.jpg",
+    },
+  ];
+
   return (
     <motion.div
       className="md:w-[60%] xl:w-[40%] h-full overflow-y-scroll bg-slate-800 absolute top-0 right-0"
@@ -19,39 +54,34 @@ export default function SearchOverlay(props: Props) {
       animate={{ x: showSearch ? 0 : 1000 }}
       transition={{ duration: 0.25 }}
     >
-      <div className="pl-[1rem] flex items-center gap-[1.2rem] py-[0.8rem]">
-        <div onClick={() => setShowSearch(false)}>
-          <HiArrowLeft className="text-gray-300 p-[0.7rem] hover:bg-gray-700 duration-200 min-h-[3.5rem] min-w-[3.5rem] cursor-pointer rounded-full" />
-        </div>
-        <form
-          className="rounded-3xl relative px-[1rem] flex items-center py-[0.8rem] bg-gray-900 border-[1px] duration-200"
-          style={{ borderColor: inputIsFocused ? mediumPurple : gray }}
-        >
-          <HiMagnifyingGlass
-            className="text-3xl mr-[1.3rem] duration-200 absolute left-[0.5rem] top-[0.9rem]"
-            style={{ color: inputIsFocused ? mediumPurple : gray }}
-          />
-          <input
-            type="text"
-            name="chatQuery"
-            className="flex-1 bg-transparent outline-none text-2xl font-medium px-[2.5rem]"
-            onFocus={() => setInputIsFocused(true)}
-            onBlur={() => setInputIsFocused(false)}
-            onChange={(e) => setQuery(e.currentTarget.value)}
-          />
-          <IoMdClose
-            className="text-4xl ml-[1rem] duration-200 absolute right-[0.5rem] top-[0.75rem] cursor-pointer"
-            style={{
-              color: inputIsFocused ? mediumPurple : gray,
-              opacity: query ? 1 : 0,
-              pointerEvents: query ? "all" : "none",
-            }}
-            onClick={() => setQuery("")}
-          />
-        </form>
-        <div>
-          <AiOutlineCalendar className="text-gray-300 p-[0.7rem] hover:bg-gray-700 duration-200 min-h-[3.5rem] min-w-[3.5rem] cursor-pointer rounded-full" />
-        </div>
+      <Header query={query} setQuery={setQuery} setShowSearch={setShowSearch} />
+      <div className="pt-[2rem]">
+        <h3 className="text-xl text-gray-300 font-medium  pl-[3rem]">{`${"200"} messages found`}</h3>
+        <ul className="overflow-y-scroll w-full px-[1rem] mt-[1.5rem]">
+          {messages.map((msg, i) => {
+            return (
+              <li
+                key={i}
+                className="hover:bg-gray-700 duration-300 rounded-xl py-[0.6rem] pl-[0.6rem] pr-[1.3rem] flex gap-[1rem] w-full cursor-pointer  "
+              >
+                <div className="w-[5rem] h-[5rem] rounded-full overflow-hidden">
+                  <img
+                    src={msg.profileImg}
+                    alt="avatar"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="flex flex-col justify-center gap-[0.2rem] flex-1">
+                  <div className="flex justify-between">
+                    <h5 className="text-2xl font-semibold">{msg.sender}</h5>
+                    <span className="text-md ">{msg.date}</span>
+                  </div>
+                  <p className="text-xl font-normal">{msg.content}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </motion.div>
   );
