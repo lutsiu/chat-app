62d7b2fec37ab6e259a3d70bec50422122d88e34
lutsiu <oleksandr.lutsiuk.1603@gmail.com>
2023-10-10 11:43:03 +0200

    and the same for file loader, if file is image

--- frontend/src/components/Chat/ChatBody/Messages/Message/FileLoader/index.tsx
+++ frontend/src/components/Chat/ChatBody/Messages/Message/FileLoader/index.tsx
@@ -1,24 +1,19 @@
-import { createPortal } from "react-dom";
 import { IFile, IMessage } from "../../../../../../interfaces/models";
 import getSizeOfFile from "../../../../../../utils/getSizeOfFile";
-import MediaOverlay from "../../../Overlays/MediaOverlay";
-import { useState } from "react";
-import {AiFillFile} from 'react-icons/ai';
+import { AiFillFile } from "react-icons/ai";
 import downloadFile from "../../../../../../utils/downloadFile";
-import { useSelector } from "react-redux";
-import { ReduxState } from "../../../../../../interfaces/redux";
+import { useDispatch } from "react-redux";
+import { setShowMediaOverlay } from "../../../../../../state/chatUI";
 interface Props {
   file: IFile;
   message: IMessage;
-
 }
 
 export default function FileLoader(props: Props) {
-  const {chatId} = useSelector((state: ReduxState) => state.chat);
-  const { file, message} = props;
+  const { file, message } = props;
   const { fileName, filePath, fileSize, fileType } = file;
-  const [showMediaOverlay, setShowMediaOverlay] = useState(false);
   const sizeToShow = getSizeOfFile(fileSize);
+  const dispatch = useDispatch();
   let thumbNail: React.ReactNode;
   if (fileType.includes("image")) {
     thumbNail = (
@@ -28,15 +23,17 @@ export default function FileLoader(props: Props) {
       />
     );
   }
-  if (!fileType.includes('image')) {
-    thumbNail = <div className="w-full h-full bg-purple-400 flex items-center justify-center">
-      <AiFillFile className="w-[50%] h-[50%]"/>
-    </div>
+  if (!fileType.includes("image")) {
+    thumbNail = (
+      <div className="w-full h-full bg-purple-400 flex items-center justify-center">
+        <AiFillFile className="w-[50%] h-[50%]" />
+      </div>
+    );
   }
-  
+
   async function handleInteractWithFile() {
     if (fileType.includes("image")) {
-      setShowMediaOverlay(true);
+      dispatch(setShowMediaOverlay({ file, message, showOverlay: true }));
     } else {
       downloadFile(file);
     }
@@ -55,15 +52,6 @@ export default function FileLoader(props: Props) {
           <span className="text-xl font-normal">{sizeToShow}</span>
         </div>
       </div>
-      {createPortal(
-        <MediaOverlay
-          file={file}
-          setShowOverlay={setShowMediaOverlay}
-          showOverlay={showMediaOverlay}
-          chatId={chatId}
-          message={message}
-        />
-      , document.getElementById('overlay') as HTMLElement)}
     </>
   );
 }
